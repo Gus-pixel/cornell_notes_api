372a6538f98dc3af8548d67315d97b9cdec6ab19
Tiago Daniel <tiago.d.santos@hotmail.com>
2025-06-09 23:28:49 -0300

    Corrigindo problema com campos vazios

--- src/validators/folhaSchema.ts
+++ src/validators/folhaSchema.ts
@@ -2,10 +2,10 @@ import { z } from 'zod';
 
 export const folhaSchema = z.object({
   usuarioId: z.string().uuid(),
-  materia: z.string().optional(),
-  titulo: z.string().optional(),
-  resumo: z.string().optional(),
-  palavras_chave: z.array(z.string()).optional(),
-  anotacoes_relevantes: z.array(z.string()).optional(),
-  cores_personalizadas: z.any().optional(),
+  materia: z.string().optional().default(''),
+  titulo: z.string().optional().default(''),
+  resumo: z.string().optional().default(''),
+  palavras_chave: z.array(z.string()).optional().default([]),
+  anotacoes_relevantes: z.array(z.string()).optional().default([]),
+  cores_personalizadas: z.any().optional().default(null),
 });
