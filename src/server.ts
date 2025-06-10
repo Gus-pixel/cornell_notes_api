9422e093f259ef245f8f664cffdb7e6f50eecb4a
Tiago Daniel <tiago.d.santos@hotmail.com>
2025-06-09 22:39:31 -0300

    Adicionando porta

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ app.use(express.json());
 
 app.use('/api', userRoutes);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
 });
